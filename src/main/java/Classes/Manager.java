35b209f75c8da9cd5d3e3b5521602ea7bead7164
roshmelcreer123 <lr.rcreer@mmdc.mcl.edu.ph>
2024-05-02 14:39:11 +0800

    Added attributes, constructor, getters, and setters for Manager class

--- src/main/java/Classes/Manager.java
+++ src/main/java/Classes/Manager.java
@@ -10,4 +10,53 @@ package Classes;
  */
 public class Manager {
     
+    // Manager Attributes
+    private String teamID;
+    private int teamSize;
+    private String reportingTo;
+    
+    // Constructor
+    
+    public Manager(String teamID, int teamSize, String reportingTo){
+        this.teamID = teamID;
+        this.teamSize = teamSize;
+        this.reportingTo = reportingTo;
+    }
+    
+    // Getters
+    public String getTeamID(){
+        return teamID;
+    }
+    
+    public int getTeamSize(){
+        return teamSize;
+    }
+    
+    public String getReportingTo(){
+        return reportingTo;
+    }
+ 
+    // Setters
+    public void setTeamID(String teamID){
+        this.teamID = teamID;
+    }
+    
+    public void setTeamSize(int teamSize){
+        this.teamSize = teamSize;
+    }
+    
+    public void setReportingTo(String reportingTo){
+        this.reportingTo = reportingTo;
+    }
+    
+    // Empty Methods
+    
+    public void approveLeaveRequest(){}
+    public void rejectLeaveRequest(){}
+    public void viewTeamLeaveRequest(){}
+    public void approveOvertimeRequests(){}
+    public void rejectOvertimeRequests(){}
+    public void assignTasks(){}
+    public void viewTeamPerformance(){}
+    
 }
