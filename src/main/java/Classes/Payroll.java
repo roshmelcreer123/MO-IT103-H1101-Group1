b5cc59fe91746f69fda0d7c437127cdc664c23f4
cajucom-ms <lr.mscajucom@mmdc.mcl.edu.ph>
2024-05-03 08:54:28 +0200

    HR and Payroll classes fixed

--- src/main/java/Classes/Payroll.java
+++ src/main/java/Classes/Payroll.java
@@ -8,6 +8,50 @@ package Classes;
  *
  * @author martin
  */
+
+import java.util.Date;
+
 public class Payroll {
     
-}
+    // Payroll Attributes
+    private String payrollSystemID;
+    private Date lastProcessedMonth;
+    
+    // Constructor
+    public Payroll(String payrollSystemID, Date lastProcessedMonth) {
+        this.payrollSystemID = payrollSystemID;
+        this.lastProcessedMonth = lastProcessedMonth;
+        
+    // Payroll Getters
+    }
+    
+    public String getPayrollSystemID() {
+        return payrollSystemID;
+    }
+    
+    public Date getLastProcessedMonth() {
+        return lastProcessedMonth;
+    
+        
+    // Payroll Setters
+    }
+
+    public void setPayrollSystemID(String payrollSystemID) {
+        this.payrollSystemID = payrollSystemID;
+    }
+    
+    public void setLastProcessedMonth(Date lastProcessedMonth) {
+        this.lastProcessedMonth = lastProcessedMonth;
+    }
+    
+    
+    // Empty methods
+    public void updateSalary() {}
+    public void processEndOfMonth() {}
+    public void viewPayrollReports() {}
+    public void calculateDeductions() {}
+    public void issuePayslips() {}
+    public void manageEmployeeBenefits() {}
+    
+    
+    }
