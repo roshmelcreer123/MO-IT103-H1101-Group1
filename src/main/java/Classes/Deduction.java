97a07ca1ea8d9f7a122fa008663c14b186768ffd
kleinborre <131511662+kleinborre@users.noreply.github.com>
2024-05-03 17:01:51 +0800

    Modified Methods of Deduction Class

--- src/main/java/Classes/Deduction.java
+++ src/main/java/Classes/Deduction.java
@@ -130,4 +130,13 @@ public class Deduction {
         this.taxRates = taxRates;
     }
     
+    //  Empty Methods based on Diagram
+    
+    public void updateBasicSalary(){}
+    public void updateAllowance(){}
+    public void updateGrossSemiMonthlyRate(){}
+    public void calculateDeductions(){}
+    public void calculateNetSalary(){}
+    public void generatePayslip(){}
+        
 }
