7a8c0c778c5161a629e97f1495d33a035a5c2319
kleinborre <131511662+kleinborre@users.noreply.github.com>
2024-05-03 16:46:02 +0800

    Added Attributes/Methods for Task Class

--- src/main/java/Classes/Task.java
+++ src/main/java/Classes/Task.java
@@ -1,8 +1,79 @@
 package Classes;
+import java.util.Date;
+
 /**
- * 
  * @author Oliver
+ * Last Updated: May 05, 2024 | 16:45
  */
+
 public class Task {
-       //Nothing to put yet empty temporarily.
+       
+    // Task Attributes
+    
+    private int taskId;
+    private String title;
+    private String description;
+    private String status;
+    private String assignedTo;
+    private Date dueDate;
+    
+    //  Task Constructors
+
+    public Task(int taskId, String title, String description, String status, String assignedTo, Date dueDate) {
+        this.taskId = taskId;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.assignedTo = assignedTo;
+        this.dueDate = dueDate;
+    }
+    
+    //  Task Getters
+    
+    public int getTaskId(){
+        return taskId;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    public String getStatus(){
+        return status;
+    }
+    
+    public String getAssignedTo(){
+        return assignedTo;
+    }
+    
+    public Date getDueDate(){
+        return dueDate;
+    }
+    
+    //  Task Setters
+    
+    public void setTitle(String title){
+        this.title = title;
+    }
+    
+    public void setDescription(String description){
+        this.description = description;
+    }
+    
+    public void setStatus(String status){
+        this.status = status;
+    }
+    
+    public void setAssignedTo(String assignedTo){
+        this.assignedTo = assignedTo;
+    }
+    
+    public void setDate(Date dueDate){
+        this.dueDate = dueDate;
+    }
+    
 }
